57354b89a333f823a4682ff3f2a54bfee5936086
miatia1025 <miatia1025@gmail.com>
2023-04-03 00:28:18 +0900

    a

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js'); 
 
 token = process.env.BOT_TOKEN
 
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
