a58d8282b6f7196debd3c30c1ec37bfd1d9211f5
miatia1025 <miatia1025@gmail.com>
2023-04-05 19:18:52 +0900

    shogfhsdoighoirsd

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
 });
 
 
-client.login(token);
+//client.login(token);
 
 app.post('/interactions', verifyKeyMiddleware(public_key), async (req, res) => {
 
